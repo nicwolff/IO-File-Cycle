32ebf83777004a3c54fc2a264153fdb11eb4647a
Nic Wolff <nic@angel.net>
2012-10-04 21:29:20 -0400

    Require IO::File in Build.PL

--- lib/IO/File/Cycle.pm
+++ lib/IO/File/Cycle.pm
@@ -11,12 +11,11 @@ IO::File::Cycle - Easily split output file while writing
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
-
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
